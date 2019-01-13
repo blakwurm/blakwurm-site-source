316354cc8763798dfa302e467dde3f06d5596062
Alex Chythlook <zaphodious@users-MacBook-Pro.local>
2019-01-12 15:21:21 -0900

    Get the 80s console theme really "on"

--- template/js/main.js
+++ template/js/main.js
@@ -1,10 +1,13 @@
 let af;
 const flickerColors = ['#eef1', '#fff1', '#fef1', '#fea1', '#fff1', '#eee1', '#0aa1']
+const textColors = ['#fc9500ff', '#fc9500ff', '#fc9500ff', '#fc9500ff', '#fc9500ff', '#fc9500ff', '#fc9500ff']
+const glowColors = ['#fcf000', '#fcf000', '#fcf000', '#fcf000', '#fcf000', '#fcf000', '#fcf000']
 const framesBetween = 3
 let framecounter = 0;
+let c = document.querySelector('#maskcanvas');
+let con = c.getContext('2d');
+let b = document.querySelector('body');
 function renderCRTeffect() {
-    let c = document.querySelector('#maskcanvas');
-    let con = c.getContext('2d');
     con.clearRect(0,0,100,100);
     let fillinx = (framecounter%((flickerColors.length-1)/framesBetween))/framesBetween
     con.fillStyle = flickerColors[fillinx]
@@ -13,6 +16,7 @@ function renderCRTeffect() {
     if (framecounter%2) {
         con.fillRect(0, (framecounter%500)/5, 100, 3)
     }
+    // b.style.setProperty('--color-text', textColors[fillinx])
     framecounter = (framecounter+1)%1500
 }
 function raf() {
